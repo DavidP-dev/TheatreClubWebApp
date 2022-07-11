3284c794ab846e9efd58582d868a98b553857e3f
DavidP-dev <picha.mda@seznam.cz>
2022-07-11 11:13:10 +0200

    multiple changes

---
+++ src/TheatreClubWebApp.Server/ClubMembersHttpHandlers.fs
@@ -0,0 +1,38 @@
+﻿module TheatreClubWebApp.Server.ClubMembersHttpHandlers
+
+open System.Data
+open Giraffe
+open Giraffe.GoodRead
+open Fable.Remoting.Server
+open Fable.Remoting.Giraffe
+open Microsoft.AspNetCore.Http
+open Microsoft.Extensions.Logging
+open TheatreClubWebApp.Shared.API
+open FSharp.Control.Tasks
+open TheatreClubWebApp.Shared.Errors
+
+open TheatreClubWebApp.Server.Business
+
+
+
+let getService (dbConn: IDbConnection) =
+    {
+    GetClubMembers = fun _ ->
+        task {
+            return getAllClubMembers dbConn
+        }
+        |> Async.AwaitTask
+
+}
+
+
+let handler : HttpHandler =
+    let remoting logger dbConn =
+        Remoting.createApi()
+        |> Remoting.withRouteBuilder ClubMembersService.RouteBuilder
+        |> Remoting.fromValue (getService dbConn)
+        |> Remoting.withErrorHandler (Remoting.errorHandler logger)
+        |> Remoting.buildHttpHandler
+
+    Require.services<ILogger<_>,IDbConnection> remoting
+
--- src/TheatreClubWebApp.Server/WebApp.fs
+++ src/TheatreClubWebApp.Server/WebApp.fs
@@ -1,46 +1,8 @@
-﻿module TheatreClubWebApp.Server.WebApp
-
-open System.Data
+module TheatreClubWebApp.Server.WebApp
 open Giraffe
-open Giraffe.GoodRead
-open Fable.Remoting.Server
-open Fable.Remoting.Giraffe
-open Microsoft.AspNetCore.Http
-open Microsoft.Extensions.Logging
-open TheatreClubWebApp.Shared.API
-open FSharp.Control.Tasks
-open TheatreClubWebApp.Shared.Errors
-
-open TheatreClubWebApp.Server.Business
-
-
-
-let getServiceFromCtx (httpContext: HttpContext) =
-    let dbConn = httpContext.GetService<IDbConnection>()
-    {
-    GetMessage = fun success ->
-        task {
-            if success then return "Hi from Server!"
-            else return ServerError.failwith (ServerError.Exception "OMG, something terrible happened")
-        }
-        |> Async.AwaitTask
-    GetClubMembers = fun _ ->
-        task {
-            return getAllClubMembers dbConn
-        }
-        |> Async.AwaitTask
-
-}
-
-
 let webApp : HttpHandler =
-    let remoting logger =
-        Remoting.createApi()
-        |> Remoting.withRouteBuilder Service.RouteBuilder
-        |> Remoting.fromContext getServiceFromCtx
-        |> Remoting.withErrorHandler (Remoting.errorHandler logger)
-        |> Remoting.buildHttpHandler
     choose [
-        Require.services<ILogger<_>> remoting
+        ClubMembersHttpHandlers.handler
+
         htmlFile "public/index.html"
     ]
