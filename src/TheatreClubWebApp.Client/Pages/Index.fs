1a90020b8910e3dda76a57402d0b9acfbac4d1b6
picha <picha.mda@seznam.cz>
2022-08-16 14:38:04 +0200

    there is a random picture on index page now

--- src/TheatreClubWebApp.Client/Pages/Index.fs
+++ src/TheatreClubWebApp.Client/Pages/Index.fs
@@ -2,19 +2,23 @@
 
 open Feliz
 open Feliz.DaisyUI
+open Feliz.DaisyUI.Operators
 
 
 [<ReactComponent>]
 let IndexView () =
     Html.div [
         prop.className "flex flex-col gap-4 mx-14"
-        prop.children
-            [
-             Daisy.alert [
+        prop.children [
+            Daisy.alert [
                 alert.info
                 prop.className "justify-center bg-base-100"
                 prop.text "Vítej ve své divadelní databázi. V menu nahoře, klikni, na co potřebuješ."
+            ]
+            Html.img [
+                prop.className "w-30 w-30"
+                ++ mask.star2
+                prop.src "https://source.unsplash.com/random/400x400/?performance"
+            ]
         ]
-
     ]
-]
