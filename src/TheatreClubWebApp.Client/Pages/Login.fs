9ff7b9d6800017cb3c59d6c11234b2b2911a41d5
picha <picha.mda@seznam.cz>
2022-09-07 15:09:08 +0200

    modified login page

--- src/TheatreClubWebApp.Client/Pages/Login.fs
+++ src/TheatreClubWebApp.Client/Pages/Login.fs
@@ -46,59 +46,63 @@ let LoginView () =
     let state,dispatch = React.useElmish(init, update, [| |])
 
     let imgSrc = importDefault "../public/img/pointdown_1_1.jpg"
-
-    Html.div [
-        prop.className "flex flex-col gap-4 mx-auto"
+    Html.form [
+        prop.onSubmit
+            (fun ev ->
+                ev.preventDefault()
+                FormSubmitted |> dispatch
+               )
         prop.children [
-            Daisy.navbar [
-                prop.className "mb-4 shadow-sm bg-base-200 rounded-box"
-            ]
             Html.div [
-                prop.className "flex justify-center"
+                prop.className "flex flex-col gap-4 mx-auto"
                 prop.children [
-                    Daisy.formControl [
-                        prop.className "flex gap-4"
+                    Daisy.navbar [
+                        prop.className "mb-4 shadow-sm bg-base-200 rounded-box"
+                    ]
+                    Html.div [
+                        prop.className "flex justify-center"
                         prop.children [
-                            Html.div [
-                                prop.className "flex justify-center"
+                            Daisy.formControl [
+                                prop.className "flex gap-4"
                                 prop.children [
-                                    Html.img [
-                                        prop.className "h-80 w-80"
-                                        ++ mask.squircle
-                                        prop.src imgSrc
+                                    Html.div [
+                                        prop.className "flex justify-center"
+                                        prop.children [
+                                            Html.img [
+                                                prop.className "h-80 w-80"
+                                                ++ mask.squircle
+                                                prop.src imgSrc
+                                            ]
+                                        ]
+                                    ]
+                                    Daisy.alert [
+                                        alert.info
+                                        prop.className "justify-center bg-base-100"
+                                        prop.text "Ahoj Romčo, zadej svůj kód, aby ses dostala k přísně střeženým údajům členů divadelního klubu."
+                                    ]
+                                    match state.Error with
+                                    | Some err ->
+                                        Daisy.alert [
+                                            alert.error
+                                            prop.className "justify-center bg-base-100"
+                                            prop.text err
+                                        ]
+                                    | None -> Html.none
+                                    Daisy.label [Daisy.labelText "Heslo hýr:"]
+                                    Daisy.input [
+                                        prop.type'.password
+                                        input.bordered
+                                        prop.placeholder "Heslo"
+                                        prop.value state.Password
+                                        prop.onChange (fun p -> PasswordChanged p |> dispatch)
+                                    ]
+                                    Daisy.button.submit [
+                                        button.outline
+                                        button.primary
+                                        button.lg
+                                        prop.value "Potvrď heslo"
                                     ]
                                 ]
-                            ]
-                            Daisy.alert [
-                                alert.info
-                                prop.className "justify-center bg-base-100"
-                                prop.text "Ahoj Romčo, zadej svůj kód, aby ses dostala k přísně střeženým údajům členů divadelního klubu."
-                            ]
-                            match state.Error with
-                            | Some err ->
-                                Daisy.alert [
-                                    alert.error
-                                    prop.className "justify-center bg-base-100"
-                                    prop.text err
-                                ]
-                            | None -> Html.none
-                            Daisy.label [Daisy.labelText "Heslo hýr:"]
-                            Daisy.input [
-                                prop.type'.password
-                                input.bordered
-                                prop.placeholder "Heslo"
-                                prop.value state.Password
-                                prop.onChange (fun p -> PasswordChanged p |> dispatch)
-                            ]
-                            Daisy.button.submit [
-                                button.outline
-                                button.primary
-                                button.lg
-                                prop.value "Potvrď heslo"
-                                prop.onClick (fun ev ->
-                                    ev.preventDefault()
-                                    FormSubmitted |> dispatch
-                                )
                             ]
                         ]
                     ]
